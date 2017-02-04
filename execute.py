900389beb665e90d9234737cb1b76f84a9af8865
Alex Wellnitz <alex@Alexs-MBP.home>
2017-02-04 19:19:26 +0100

    adding function 'find chords'

--- execute.py
+++ execute.py
@@ -1,10 +1,13 @@
 
 
 import tensorflow as tp
+import imp
+
+findChords = imp.load_source('module.name', 'classes/findChords.py')
+
+findChords.test()
 
 #Config
-path = "Music_Lib/"
+music_path = "Music_Lib/"
 
-def get_all_TopHit_Music(path){
-
-}
+#def get_all_TopHit_Music(music_path)
